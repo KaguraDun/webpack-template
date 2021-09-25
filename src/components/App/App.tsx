ff7a080c89295fef0ae92bf8012ddf25cb75a7a8
Vasily Kovnev <v.k95@yandex.ru>
2021-09-25 18:34:13 +0300

    feat: remove icon in app component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 import s from './App.scss';
-import Cat from './happy.svg';
 
 const App = () => (
   <div className={s.container}>
-    <Cat />
+    Hello
   </div>
 );
 
